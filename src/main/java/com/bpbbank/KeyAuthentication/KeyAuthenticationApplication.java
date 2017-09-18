c1b026d8280e8e6bd3b32075079fd85bc896a197
Rinor Jashari <rinor.jashari@bpb.com>
2017-09-18 15:16:23 +0200

    modifikim

---
+++ src/main/java/com/bpbbank/KeyAuthentication/KeyAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.bpbbank.KeyAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyAuthenticationApplication.class, args);
+	}
+}
