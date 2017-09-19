9930dad71169d2fd90ab92d2dc085e3c60aeffd6
Rinor Jashari <rinor.jashari@bpb.com>
2017-09-19 16:40:39 +0200

    modifikime ne web app

--- src/main/java/com/bpbbank/controllers/HomeController.java
+++ src/main/java/com/bpbbank/controllers/HomeController.java
@@ -1,14 +1,22 @@
 package com.bpbbank.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String getHome() {
+	public String getHome(Model model) {
+                model.addAttribute("listaEcelesave", "RINOR");
 		System.out.println("AKAKUNKAKA");
-		return "home1";
+		return "s";
 	}
+        @GetMapping("/addKey")
+        public String getAddKey(Model model){
+            
+            model.addAttribute("key","Celesi1");
+            return "addKey";
+        }
 }
