d04776676e3ce07444e155b83676574457819894
Ngadhnjim Berani - BPB <ngadhnjim.berani@bpbbank.com>
2017-10-08 21:12:20 +0200

    b

--- src/main/java/com/bpbbank/WebSecurityConfig.java
+++ src/main/java/com/bpbbank/WebSecurityConfig.java
@@ -1,22 +1,31 @@
 package com.bpbbank;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.authentication.encoding.PlaintextPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	// @Value("${ad.domain}")
-	private String AD_DOMAIN;
+	private String AD_DOMAIN = "LDAP://DC=bpb,DC=com";
 
 	// @Value("${ad.url}")
-	private String AD_URL;
-
+	private String AD_URL = "ldap://hq-dc01.bpb.com:389/dc=bpb,dc=com";
+/*
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
@@ -29,4 +38,54 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.withUser("rinori").password("Banka123!").roles("USER").and().withUser("gagi").password("Banka123!")
 				.roles("USER");
 	}
+	*/
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.ldapAuthentication()
+//			.userDnPatterns("cn={0},OU=Sektori i Databazes dhe zhvillimit,OU=Departamenti i IT,OU=HQ Manage Users,OU=Managed Users")
+//			.userDnPatterns("sAMAccountName={0},OU=person")
+//			.groupSearchBase("ou=groups")
+			.contextSource()
+				.url("ldap://hq-dc01.bpb.com:389/dc=bpb,dc=com")
+				.and()
+			.passwordCompare()
+				.passwordEncoder(new LdapShaPasswordEncoder())
+				.passwordAttribute("userPassword");
+	}
+	
+	
+	/*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder.authenticationProvider(activeDirectoryLdapAuthenticationProvider()).userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(Arrays.asList(activeDirectoryLdapAuthenticationProvider()));
+    }
+    @Bean
+    public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+        ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider(AD_DOMAIN, AD_URL);
+        provider.setConvertSubErrorCodesToExceptions(true);
+        provider.setUseAuthenticationRequestCredentials(true);
+        
+        return provider;
+    }*/
 }
